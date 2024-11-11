c531760ec28f441614fc9c38f211e588049b83c3
Rubèn <numbworks@gmail.com>
2024-11-11 14:21:24 +0000

    #37 RLDataFrameHelperTestCase, added (100%).

--- tests/nwreadinglisttests.py
+++ tests/nwreadinglisttests.py
@@ -14,7 +14,7 @@ from unittest.mock import Mock, call, patch
 
 # LOCAL MODULES
 sys.path.append(os.path.dirname(__file__).replace('tests', 'src'))
-from nwreadinglist import RLID, _MessageCollection, DefaultPathProvider, MDInfo, RLSummary, YearProvider, SettingBag, ComponentBag
+from nwreadinglist import RLCN, RLID, _MessageCollection, DefaultPathProvider, MDInfo, RLDataFrameHelper, RLSummary, YearProvider, SettingBag, ComponentBag
 from nwreadinglist import RLDataFrameFactory, RLMarkdownFactory
 from nwshared import MarkdownHelper, Formatter, FilePathManager
 
@@ -36,35 +36,27 @@ class SupportMethodProvider():
             dtype_names.append(dtype.name)
 
         return dtype_names
+class ObjectMother():
+
+    '''Collects all the DTOs required by the unit tests.'''
+
+    @staticmethod
+    def create_default_sa_by_2024_df() -> DataFrame:
+
+        default_df : DataFrame = pd.DataFrame(
+            {
+                "Month": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                "2024": ["0 (0)", "0 (0)", "0 (0)", "0 (0)", "0 (0)", "0 (0)", "0 (0)", "0 (0)", "0 (0)", "0 (0)", "0 (0)", "0 (0)"]
+            },
+            index=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        )
+
+        default_df = default_df.astype({"Month": int})
+        default_df = default_df.astype({"2024": str})
+
+        return default_df
 
 # TEST CLASSES
-class DefaultPathProviderTestCase(unittest.TestCase):
-
-    def test_getdefaultreadinglistpath_shouldreturnexpectedpath_wheninvoked(self):
-        
-        '''"C:/project_dir/src/" => "C:/project_dir/data/Reading List.xlsx"'''
-
-        # Arrange
-        expected : str = "C:/project_dir/data/Reading List.xlsx"
-
-        # Act
-        with patch.object(os, 'getcwd', return_value="C:/project_dir/src/") as mocked_context:
-            actual : str = DefaultPathProvider().get_default_reading_list_path()
-
-        # Assert
-        self.assertEqual(expected, actual)
-class YearProviderTestCase(unittest.TestCase):
-
-    def test_getallyears_shouldreturnexpectedlist_wheninvoked(self):
-
-        # Arrange
-        expected : list[int] = [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
-
-        # Act
-        actual : list[int] = YearProvider().get_all_years()
-
-        # Assert
-        self.assertEqual(expected, actual)
 class MDInfoTestCase(unittest.TestCase):
     
     def test_mdinfo_shouldinitializeasexpected_wheninvoked(self):
@@ -276,6 +268,199 @@ class MessageCollectionTestCase(unittest.TestCase):
         
         # Assert
         self.assertEqual(actual, expected)
+class DefaultPathProviderTestCase(unittest.TestCase):
+
+    def test_getdefaultreadinglistpath_shouldreturnexpectedpath_wheninvoked(self):
+        
+        '''"C:/project_dir/src/" => "C:/project_dir/data/Reading List.xlsx"'''
+
+        # Arrange
+        expected : str = "C:/project_dir/data/Reading List.xlsx"
+
+        # Act
+        with patch.object(os, 'getcwd', return_value="C:/project_dir/src/") as mocked_context:
+            actual : str = DefaultPathProvider().get_default_reading_list_path()
+
+        # Assert
+        self.assertEqual(expected, actual)
+class YearProviderTestCase(unittest.TestCase):
+
+    def test_getallyears_shouldreturnexpectedlist_wheninvoked(self):
+
+        # Arrange
+        expected : list[int] = [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
+
+        # Act
+        actual : list[int] = YearProvider().get_all_years()
+
+        # Assert
+        self.assertEqual(expected, actual)
+class RLDataFrameHelperTestCase(unittest.TestCase):
+
+    @parameterized.expand([
+        [0, 0, "0 (0)"],
+        [13, 5157, "13 (5157)"]
+    ])
+    def test_formatreadingstatus_shouldreturnexpectedstring_wheninvoked(self, books : int, pages : int, expected : str):
+        
+        # Arrange
+        # Act
+        actual : str = RLDataFrameHelper().format_reading_status(books = books, pages = pages)
+
+        # Assert
+        self.assertEqual(expected, actual)
+
+    def test_getdefaultsabyyear_shouldreturnexpecteddataframe_wheninvoked(self):
+        
+        # Arrange
+        expected : DataFrame = ObjectMother().create_default_sa_by_2024_df()
+
+        # Act
+        actual : DataFrame = RLDataFrameHelper().get_default_sa_by_year(read_year = 2024)
+
+        # Assert
+        assert_frame_equal(expected, actual)
+
+    @parameterized.expand([
+        ["0 (0)", 0],
+        ["13 (5157)", 13]
+    ])
+    def test_extractbooksfromtrend_shouldreturnexpectedint_wheninvoked(self, trend : str, expected : int):
+        
+        # Arrange
+        # Act
+        actual : int = RLDataFrameHelper().extract_books_from_trend(trend = trend)
+
+        # Assert
+        self.assertEqual(expected, actual)    
+
+    @parameterized.expand([
+        [13, 16, "↑"],
+        [16, 13, "↓"],
+        [0, 0, "="]
+    ])
+    def test_gettrend_shouldreturnexpectedstring_wheninvoked(self, value_1 : int, value_2 : int, expected : str):
+        
+        # Arrange
+        # Act
+        actual : str = RLDataFrameHelper().get_trend(value_1 = value_1, value_2 = value_2)
+
+        # Assert
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ["13 (5157)", "16 (3816)", "↑"],
+        ["16 (3816)", "13 (5157)", "↓"],
+        ["0 (0)", "0 (0)", "="]
+    ])
+    def test_gettrendbybooks_shouldreturnexpectedstring_wheninvoked(self, trend_1 : str, trend_2 : str, expected : str):
+        
+        # Arrange
+        # Act
+        actual : str = RLDataFrameHelper().get_trend_by_books(trend_1 = trend_1, trend_2 = trend_2)
+
+        # Assert
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ["2016", "2016"],
+        ["↕1", "↕"]
+    ])
+    def test_tryconsolidatetrendcolumnname_shouldreturnexpectedstring_wheninvoked(self, column_name : str, expected : str):
+        
+        # Arrange
+        # Act
+        actual : str = RLDataFrameHelper().try_consolidate_trend_column_name(column_name = column_name)
+
+        # Assert
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ["13 (5157)", 5157],
+        ["0 (0)", 0]
+    ])
+    def test_extractpagesfromtrend_shouldreturnexpectedint_wheninvoked(self, trend : str, expected : int):
+        
+        # Arrange
+        # Act
+        actual : int = RLDataFrameHelper().extract_pages_from_trend(trend = trend)
+
+        # Assert
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ["2016", "2016_Books"]
+    ])
+    def test_formatyearbookscolumnname_shouldreturnexpectedstring_wheninvoked(self, year_cn : str, expected : str):
+        
+        # Arrange
+        # Act
+        actual : str = RLDataFrameHelper().format_year_books_column_name(year_cn = year_cn)
+
+        # Assert
+        self.assertEqual(expected, actual)
+    
+    @parameterized.expand([
+        ["2016", "2016_Pages"]
+    ])
+    def test_formatyearpagescolumnname_shouldreturnexpectedstring_wheninvoked(self, year_cn : str, expected : str):
+        
+        # Arrange
+        # Act
+        actual : str = RLDataFrameHelper().format_year_pages_column_name(year_cn = year_cn)
+
+        # Assert
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ["2016_Books", "2016"],
+        ["2016_Pages", "2016"]
+    ])
+    def test_extractyearfromcolumnname_shouldreturnexpectedstring_wheninvoked(self, column_name : str, expected : str):
+        
+        # Arrange
+        # Act
+        actual : str = RLDataFrameHelper().extract_year_from_column_name(column_name = column_name)
+
+        # Assert
+        self.assertEqual(expected, actual)
+    
+    @parameterized.expand([
+        [1447.14, 2123.36, "↑"],
+        [2123.36, 1447.14, "↓"],
+        [0, 0, "="]
+    ])
+    def test_gettrendwhenfloat64_shouldreturnexpectedstring_wheninvoked(self, value_1 : float64, value_2 : float64, expected : str):
+        
+        # Arrange
+        # Act
+        actual : str = RLDataFrameHelper().get_trend_when_float64(value_1 = value_1, value_2 = value_2)
+
+        # Assert
+        self.assertEqual(expected, actual)
+
+    def test_createreadyearsdataframe_shouldreturnexpecteddataframe_whenreadyears(self):
+        
+        # Arrange
+        read_years : list[int] = [2020, 2021, 2022]
+        expected : DataFrame = pd.DataFrame(data = [2020, 2021, 2022], columns = [RLCN.READYEAR])
+
+        # Act
+        actual : DataFrame = RLDataFrameHelper().create_read_years_dataframe(read_years = read_years)
+
+        # Assert
+        assert_frame_equal(actual, expected)
+    def test_createreadyearsdataframe_shouldreturnemptydataframe_whenemptyreadyears(self):
+        
+        # Arrange
+        read_years : list[int] = []
+        expected : DataFrame = pd.DataFrame(data = [], columns = [RLCN.READYEAR])
+
+        # Act
+        actual : DataFrame = RLDataFrameHelper().create_read_years_dataframe(read_years = read_years)
+
+        # Assert
+        assert_frame_equal(actual, expected)
 
 
 # MAIN
